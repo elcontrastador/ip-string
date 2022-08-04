85de1f405555dfaddcad9e622206b16afb6aa12b
Tyler Arrigoni <tyler@oneif.net>
2022-08-03 18:37:59 -0700

    ipDecToBin function

--- src/ip.ts
+++ src/ip.ts
@@ -1,4 +1,4 @@
-//in: 'xxx.xxx.xxx.xxx', out: '101010101010101010011010101010101' (32bits as str)
+//in: 'xxx.xxx.xxx.xxx', out: '1010101010101010100110101' (32bits as str)
 export function ipDecToBin (ip: string): string {
     const ipA = ip.split('.').map(oct => BigInt(oct));
     const bin = (ipA[0] << 24n | ipA[1] << 16n | ipA[2] << 8n | ipA[3]);
