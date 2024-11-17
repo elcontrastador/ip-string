e2b9eaf3e329f5dfcf7c1c6426d7e2e23f0243ac
Tyler Arrigoni <tyler@oneif.net>
2024-11-16 16:55:13 -0800

    updated npm packages

--- bin/t1.js
+++ bin/t1.js
@@ -1,6 +1,6 @@
 "use strict";
 exports.__esModule = true;
-var ips = require("../src/index");
+var ips = require("@elcontrastador/ip-string");
 var bin1 = ips.ipDecToBin('10.11.12.13');
 console.log("".concat(bin1, ": ").concat(bin1.length, "b"));
 var ip1 = ips.ipBinToDec(bin1);
